9d73c30356d54abb880125f29bb8b49e316a869c
EC2 Default User <kaz.f69@gmail.com>
2021-02-13 15:49:08 +0000

    user info fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,29 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :corporationes, :applies, :undertakes, :searches]
+  #before_action :require_user_logged_in, only: [:index, :show, :corporationes, :applies, :undertakes, :searches]
+  before_action :require_permission
+  
+  def require_permission
+    if logged_in?
+    elsif clogged_in?
+    else
+    end
+  end
+  
   def index
     redirect_to root_url
     #@users = User.order(id: :desc).page(params[:page]).per(25)  #User.order(id: :desc) で id の降順にユーザの一覧を取得
   end
 
   def show
-    @user = current_user
-    #@user = User.find(params[:id])
-    #@companies = Company.all
-    #@jobs = Job.all.page(params[:page]).per(5)
+    puts 'hello'
+    if logged_in?
+      @user = current_user
+    else
+      @user = User.find(params[:id])
+      #@companies = Company.all
+      #@jobs = Job.all.page(params[:page]).per(5)
+    end
+    
   end
 
   def new
